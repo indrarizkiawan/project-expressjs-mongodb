6677ff27d2d3587f83003f9aa1d9efe724b0320d
Indra Rizkiawan <indra.rizkiawan@gmail.com>
2024-09-28 15:52:05 +0700

    membuat restfull save dan memperbaiki beberapa baris code dan menambahkan method

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ mongoose
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
   res.send("hello world");
@@ -32,8 +33,15 @@ app.get("/products/create", (req, res) => {
   res.render("products/create");
 });
 
+app.post("/products", async (req, res) => {
+  const product = new Product(req.body);
+  await product.save();
+  res.redirect(`/products/${product._id}`);
+});
+
 app.get("/products/:id", async (req, res) => {
-  const product = await Product.findById(req.params.id);
+  const { id } = req.params;
+  const product = await Product.findById(id);
   res.render("products/show", { product });
 });
 
